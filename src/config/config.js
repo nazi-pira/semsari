cf47a34f972565d485d0c5b0d6b4ed20b4ffc189
Nazanin Adampira <mikael@Nasbook.local>
2021-07-08 19:46:23 +0200

    add backend url, will fix later

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,1 @@
-export const BACKEND_BASE_URL = process.env.BACKEND_BASE_URL || 'http://localhost:8080'
-
+export const BACKEND_BASE_URL = 'https://semsarii.herokuapp.com' // process.env.BACKEND_BASE_URL || 'http://localhost:8080'
